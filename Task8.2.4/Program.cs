1c67b64ce8e6156c0fc05b6af649a85178add4ef
Bochanikita <bochanikita@yandex.ru>
2022-10-12 23:52:19 +0300

    Add Task 8.2.4

---
+++ Task8.2.4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+namespace DriveManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReplaceDir();
+        }
+
+        static void ReplaceDir()
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(@"C:\\Files\test");
+            string newPath = @"C:\\Files\Trash\test";
+
+            if(!Directory.Exists(newPath))
+            {
+                dirInfo.MoveTo(newPath);
+            }
+        }
+    }
+}
