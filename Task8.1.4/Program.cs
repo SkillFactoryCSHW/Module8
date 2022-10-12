432546795b8e3bdc439cc31985495ad8fb250ebb
Bochanikita <bochanikita@yandex.ru>
2022-10-12 13:54:11 +0300

    Add project files.

---
+++ Task8.1.4/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Drive
+{
+    public Drive(string name, long space, long freeSpace)
+    {
+        Name = name;
+        Space = space; 
+        FreeSpace = freeSpace;
+    }
+
+    public string Name { get; }
+    public long Space { get; }
+    public long FreeSpace { get; }
+}
---
+++ Task8.1.5/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Folder
+{
+    public List<string> Files { get; set; } = new List<string>();
+
+    Dictionary<string, Folder> Folders = new Dictionary<string, Folder>();
+
+    public void CreateFolder(string name)
+    {
+        Folders.Add(name, new Folder());
+    }
+}
