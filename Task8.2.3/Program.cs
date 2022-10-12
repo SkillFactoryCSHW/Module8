4f1e531d0be614f8f537c16114db153786cbdd87
Bochanikita <bochanikita@yandex.ru>
2022-10-12 23:29:51 +0300

    Add task 8.2.3

---
+++ Task8.2.3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+namespace DriveManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateDir();
+            GetFilesNum();
+            DeleteDir();
+            GetFilesNum();
+        }
+
+        static void CreateDir()
+        {
+            //DirectoryInfo dirInfo = new DirectoryInfo(@"C:\\Files");
+
+
+
+            DirectoryInfo newDir = new DirectoryInfo(@"C:\\Files");
+
+            if (newDir.Exists)
+            {
+                newDir.CreateSubdirectory(@"NewDir");
+                Console.WriteLine("Dirrectory create");
+
+            }
+        }
+
+        static void DeleteDir()
+        {
+            DirectoryInfo delDir = new DirectoryInfo(@"C:\\Files\NewDir");
+
+            if (delDir.Exists)
+            {
+                delDir.Delete(true);
+                Console.WriteLine("Dirrectory delite");
+            }
+        }
+
+        static void GetFilesNum()
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(@"C:\\Files");
+
+            if (dirInfo.Exists)
+            {
+                Console.WriteLine(dirInfo.GetDirectories().Length);
+            }
+        }
+    }
+}
