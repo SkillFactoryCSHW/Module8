b532066f76af2b9f34b71527b9c0ef31e5c2f3e9
Bochanikita <bochanikita@yandex.ru>
2022-10-12 22:20:55 +0300

    Add Task 8.2.1

---
+++ Task8.2.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+namespace DriveManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        
+        static void GetFilesNum()
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(@"C:\\");
+
+            if (dirInfo.Exists)
+            {
+                Console.WriteLine(dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length);
+            }
+        }
+    }
+}
