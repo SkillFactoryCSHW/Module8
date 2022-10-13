6ffd108564e357125a8fcd9c0804b8167421cfb1
Bochanikita <bochanikita@yandex.ru>
2022-10-13 22:26:15 +0300

    Add Task 8.6.1

---
+++ Task 8.6.1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+namespace DriveManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FolderName = @"C\\Files\Test";
+            clearFolder(FolderName);
+        }
+
+        static void clearFolder(string FolderName)
+        {
+            try
+            {
+                if (Directory.Exists(FolderName))
+                {
+                    DirectoryInfo dir = new DirectoryInfo(FolderName);
+
+                    foreach (FileInfo fi in dir.GetFiles())
+                    {
+                        fi.Delete();
+                    }
+
+                    foreach (DirectoryInfo di in dir.GetDirectories())
+                    {
+                        clearFolder(di.FullName);
+                        di.Delete();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Ошибка: {e}");
+            }
+        }
+        }
+    }
+}
